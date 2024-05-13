8b8be3b032f1b1fe2776cc46d447f87f109fb3f8
KingJiwon <wldnjs0401@naver.com>
2024-05-13 22:02:36 +0900

    main_highlight_publish

--- app/page.js
+++ app/page.js
@@ -1,3 +1,73 @@
+import main from '../styles/main.module.scss';
+
 export default function Home() {
-  return <h1>Main</h1>;
+  return (
+    <div className={main.main_container}>
+      <div className={main.popular_highlight_container}>
+        <div className={main.popular_highlight_header}>
+          <img
+            src="/icon/teams/arsenal.svg"
+            alt="팀 로고"
+            className={main.popular_highlight_header_logo}
+          />
+          <p className={main.popular_highlight_header_title}>
+            MADDD님의 아스널 하이라이트
+          </p>
+          <p className={main.popular_highlight_header_info}>추천10 조회28</p>
+        </div>
+        <div className={main.popular_highlight_main}>
+          <img
+            className={main.popular_highlight_main_btn_left}
+            src="/icon/normal/left.svg"
+            alt="왼쪽 화살표"
+          />
+          <div className={main.popular_highlight_main_player}>
+            <div className={main.popular_highlight_main_player_container}>
+              <p className={main.popular_highlight_main_player_position}>FW</p>
+              <div className={main.popular_highlight_main_player_img}>
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+              </div>
+            </div>
+            <div className={main.popular_highlight_main_player_container}>
+              <p className={main.popular_highlight_main_player_position}>MF</p>
+              <div className={main.popular_highlight_main_player_img}>
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+              </div>
+            </div>
+            <div className={main.popular_highlight_main_player_container}>
+              <p className={main.popular_highlight_main_player_position}>DF</p>
+              <div className={main.popular_highlight_main_player_img}>
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+              </div>
+            </div>
+            <div className={main.popular_highlight_main_player_container}>
+              <p className={main.popular_highlight_main_player_position}>GK</p>
+              <div className={main.popular_highlight_main_player_img}>
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+              </div>
+            </div>
+          </div>
+          <img
+            src="/images/test/test_img.jpg"
+            className={main.popular_highlight_main_video}
+          />
+          <img
+            className={main.popular_highlight_main_btn_right}
+            src="/icon/normal/right.svg"
+            alt="오른쪽 화살표"
+          />
+        </div>
+        <div className={main.popular_highlight_btn_container}>
+          <div className={main.popular_highlight_btn}>
+            내 구단 하이라이트 만들기
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
