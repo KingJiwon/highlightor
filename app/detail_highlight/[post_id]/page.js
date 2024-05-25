422229599c7fb59cba98c3e1b06f742fee5a6750
KingJiwon <wldnjs0401@naver.com>
2024-05-25 22:47:02 +0900

    detail title

--- app/detail_highlight/[post_id]/page.js
+++ app/detail_highlight/[post_id]/page.js
@@ -1,5 +1,19 @@
-import detail_highlight from '../../../styles/detail_highlight.module.scss';
+import detail from '../../../styles/detail_highlight.module.scss';
 
 export default function page() {
-  return <div>abc</div>;
+  return (
+    <>
+      <div className={detail.main_container}>
+        <div className={detail.back_btn_container}>
+          <p>목록으로</p>
+          <img src="/icon/normal/right.svg" />
+        </div>
+        <div className={detail.title_container}>
+          <img className={detail.title_logo} src="/icon/teams/arsenal.svg" />
+          <p className={detail.title_name}>MADDD님의 아스널 하이라이트</p>
+          <p className={detail.title_info}>추천10 조회29</p>
+        </div>
+      </div>
+    </>
+  );
 }
