9a6ef0f755d24c2711cb55c8bfdc04486d54b77a
KingJiwon <wldnjs0401@naver.com>
2024-05-26 23:32:21 +0900

    detail content

--- app/detail_highlight/[post_id]/page.js
+++ app/detail_highlight/[post_id]/page.js
@@ -13,6 +13,37 @@ export default function page() {
           <p className={detail.title_name}>MADDD님의 아스널 하이라이트</p>
           <p className={detail.title_info}>추천10 조회29</p>
         </div>
+        <div className={detail.content_container}>
+          <div className={detail.content_player_container}>
+            <div className={detail.content_player_position}>
+              <p>FW</p>
+              <p>MF</p>
+              <p>DF</p>
+              <p>GK</p>
+            </div>
+            <div className={detail.content_player_list}>
+              <div className={detail.content_player}>
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+                <p>마르틴 외데고르</p>
+              </div>
+              <div className={detail.content_player}>
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+                <p>마르틴 외데고르</p>
+              </div>
+              <div className={detail.content_player}>
+                <img src="/images/test/player.jpg" alt="선수 이미지" />
+                <p>마르틴 외데고르</p>
+              </div>
+            </div>
+          </div>
+          <div className={detail.content_highlight_container}>
+            <img
+              className={detail.content_highlight}
+              src="/images/test/test_img.jpg"
+              alt="하이라이트 "
+            />
+          </div>
+        </div>
       </div>
     </>
   );
