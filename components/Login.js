f1835efc5cf4c34817e9a7796bd971bf60a17325
KingJiwon <wldnjs0401@naver.com>
2024-07-05 15:18:03 +0900

    로그인 모달 퍼블리싱

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,37 @@
+import Link from 'next/link';
 import login from '../styles/components/modal/loginModal.module.scss';
 
 export default function Login() {
-  return <div className={login.login_container}>로그인 페이지</div>;
+  return (
+    <div className={login.login_container}>
+      <div className={login.login_inner}>
+        <Link href={'/'} className={login.login_header}>
+          HighLightor
+        </Link>
+        <form className={login.login_form}>
+          <input className={login.login_form_id} type="text" placeholder="ID" />
+          <input
+            className={login.login_form_pw}
+            type="password"
+            placeholder="비밀번호"
+          />
+          <input
+            className={login.login_form_submit}
+            type="submit"
+            value="로그인"
+          />
+        </form>
+        <div className={login.login_menu}>
+          <Link href={'/'}>회원가입</Link>
+          <Link href={'/'}>ID찾기</Link>
+          <Link href={'/'}>비밀번호 찾기</Link>
+        </div>
+        <div className={login.login_other}>
+          <div className={login.login_others}> SNS 로그인 </div>
+          <div className={login.login_others}> SNS 로그인 </div>
+          <div className={login.login_others}> SNS 로그인 </div>
+        </div>
+      </div>
+    </div>
+  );
 }
