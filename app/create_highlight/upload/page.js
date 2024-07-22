388c917af05170f6d9593d2c2772b6acc04a553e
KingJiwon <wldnjs0401@naver.com>
2024-07-22 18:21:16 +0900

    07/22

--- app/create_highlight/upload/page.js
+++ app/create_highlight/upload/page.js
@@ -2,11 +2,13 @@
 
 import Link from 'next/link';
 import { useSquad } from '@/util/context/SquadContext';
+import Image from 'next/image';
+
 import upload from '../../../styles/pages/upload.module.scss';
 
 export default function Page() {
   const { squad } = useSquad();
-  console.log(squad);
+
   return (
     <>
       <div className={upload.discriptor_container}>
@@ -20,161 +22,44 @@ export default function Page() {
       <div className={upload.uploader_container}>
         <div className={upload.uploader_inner}>
           <div className={upload.uploader_counter_container}>
-            <p className={upload.uploader_counter}>11/11</p>
+            <p className={upload.uploader_counter}>0/11</p>
           </div>
-          <div className={upload.uploader_main_container}>
-            <div className={upload.uploader_btn_container}>
-              <div className={upload.uploader_position_fw}>FW</div>
-              <div className={upload.uploader_btn_upload}>
-                <Link href={'/create_highlight/upload/search_modal/fw'}>
-                  선수 검색
-                </Link>
-                <Link href={'/'}>영상 업로드</Link>
+          {Object.keys(squad).map((position, idx) => (
+            <div key={idx} className={upload.uploader_main_container}>
+              <div className={upload.uploader_btn_container}>
+                <div className={upload.uploader_position}>{position}</div>
+                <div className={upload.uploader_btn_upload}>
+                  <Link
+                    href={`/create_highlight/upload/search_modal/${position}`}
+                  >
+                    선수 검색
+                  </Link>
+                  <Link href={'/'}>영상 업로드</Link>
+                </div>
+              </div>
+              <div className={upload.uploader_player_container}>
+                <div className={upload.uploader_player}>
+                  {squad[position].map((player) => (
+                    <>
+                      <Image
+                        width={200}
+                        height={100}
+                        className={upload.uploader_player_img}
+                        src={player.playerImg}
+                        alt={player.name}
+                      />
+                      <p className={upload.uploader_player_name}>
+                        {player.name}
+                      </p>
+                    </>
+                  ))}
+                </div>
+              </div>
+              <div className={upload.uploader_highlight}>
+                <img src="/images/test/test_img.jpg" alt="테스트 이미지" />
               </div>
             </div>
-            <div className={upload.uploader_player_container}>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="https://fco.dn.nexoncdn.co.kr/live/externalAssets/common/playersAction/p814243780.png"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="https://fco.dn.nexoncdn.co.kr/live/externalAssets/common/playersAction/p810243780.png"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-            </div>
-            <div className={upload.uploader_highlight}>
-              <img src="/images/test/test_img.jpg" alt="테스트 이미지" />
-            </div>
-          </div>
-          <div className={upload.uploader_main_container}>
-            <div className={upload.uploader_btn_container}>
-              <div className={upload.uploader_position_mf}>MF</div>
-              <div className={upload.uploader_btn_upload}>
-                <Link href={'/create_highlight/upload/search_modal/mf'}>
-                  선수 검색
-                </Link>
-                <Link href={'/'}>영상 업로드</Link>
-              </div>
-            </div>
-            <div className={upload.uploader_player_container}>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-            </div>
-            <div className={upload.uploader_highlight}>
-              <img src="/images/test/test_img.jpg" alt="테스트 이미지" />
-            </div>
-          </div>
-          <div className={upload.uploader_main_container}>
-            <div className={upload.uploader_btn_container}>
-              <div className={upload.uploader_position_df}>DF</div>
-              <div className={upload.uploader_btn_upload}>
-                <Link href={'/create_highlight/upload/search_modal/df'}>
-                  선수 검색
-                </Link>
-                <Link href={'/'}>영상 업로드</Link>
-              </div>
-            </div>
-            <div className={upload.uploader_player_container}>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-            </div>
-            <div className={upload.uploader_highlight}>
-              <img src="/images/test/test_img.jpg" alt="테스트 이미지" />
-            </div>
-          </div>
-          <div className={upload.uploader_main_container}>
-            <div className={upload.uploader_btn_container}>
-              <div className={upload.uploader_position_gk}>GK</div>
-              <div className={upload.uploader_btn_upload}>
-                <Link href={'/create_highlight/upload/search_modal/gk'}>
-                  선수 검색
-                </Link>
-                <Link href={'/'}>영상 업로드</Link>
-              </div>
-            </div>
-            <div className={upload.uploader_player_container}>
-              <div className={upload.uploader_player}>
-                <img
-                  className={upload.uploader_player_img}
-                  src="/images/test/player.jpg"
-                  alt="선수 이미지"
-                />
-                <p className={upload.uploader_player_name}>마르틴 외데고르</p>
-              </div>
-            </div>
-            <div className={upload.uploader_highlight}>
-              <img src="/images/test/test_img.jpg" alt="테스트 이미지" />
-            </div>
-          </div>
+          ))}
           <Link
             href={'/detail_highlight/1234'}
             className={upload.uploader_create_btn_container}
