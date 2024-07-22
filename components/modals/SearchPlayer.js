4c4f1ff13642bfca418d9c7ef85c506e75802849
KingJiwon <wldnjs0401@naver.com>
2024-07-22 12:31:54 +0900

    선수 검색 로딩, 검색 예외처리

--- components/modals/SearchPlayer.js
+++ components/modals/SearchPlayer.js
@@ -5,29 +5,41 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 import { getPlayerData, getSeasonData } from '@/app/apis/data';
 import searchModal from '../../styles/components/modal/searchModal.module.scss';
+import Loading from '../Loading';
 
 export default function SearchPlayer() {
   const router = useRouter();
+
+  // dataState
   const [playerData, setPlayerData] = useState([]);
   const [seasonData, setSeasonData] = useState([]);
-  const [searchData, setSearchData] = useState([]);
+  const [searchData, setSearchData] = useState(null);
+
+  // handlingState
   // eslint-disable-next-line no-unused-vars
   const [retryCount, setRetryCount] = useState({});
-  // const [loadList, setLoadList] = useState({});
+  const [loadList, setLoadList] = useState({});
+  const [isAllLoad, setIsAllLoad] = useState(true);
   const [error, setError] = useState(null);
+
+  // refs
   const inputRef = useRef(null);
 
   // 선수검색
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSearchData(null);
     // name으로 선수 고유식별자 찾기
     if (inputRef.current.value.trim() === '') {
       return setError('검색어를 입력해주세요');
     }
-    setRetryCount({});
     const searchedNameList = playerData.filter((el) =>
       el.name.includes(inputRef.current.value),
     );
+    if (searchedNameList.length === 0) {
+      setError('해당 선수를 찾을 수 없습니다');
+      return setIsAllLoad(true);
+    }
 
     // 시즌이미지 정보 추가 (고유 식별자 앞 3자리 -> 시즌id)
     const addSeason = searchedNameList.map((el) => {
@@ -41,11 +53,13 @@ export default function SearchPlayer() {
         className,
       };
     });
+    setRetryCount({});
     setError(false);
+    setIsAllLoad(false);
     return setSearchData(addSeason);
   };
 
-  // 이미지 검색
+  // 이미지 로드 핸들링
   const handleImageError = (e, id) => {
     setRetryCount((prev) => {
       const retry = (prev[id] || 0) + 1;
@@ -57,17 +71,22 @@ export default function SearchPlayer() {
         e.target.src = `https://fco.dn.nexoncdn.co.kr/live/externalAssets/common/players/p${pid}.png`;
       } else if (retry === 3) {
         e.target.src = '/images/undefined_player.png';
-        e.target.onerror = () => console.log('이미지 로드 오류');
       }
       return newRetryCount;
     });
   };
 
-  // const handleOnLoad = (id) => {
-  //   const newLoadList = { ...loadList, [id]: true };
-  //   // console.log(Object.values(newLoadList));
-  //   return setLoadList(newLoadList);
-  // };
+  // 선수 이미지 로딩
+  const handleOnLoad = (id) => {
+    const newLoadList = { ...loadList, [id]: true };
+    setLoadList(newLoadList);
+    const checkAllLoad = Object.values(newLoadList).every(
+      (value) => value === true,
+    );
+    if (checkAllLoad) {
+      setIsAllLoad(true);
+    }
+  };
 
   useEffect(() => {
     // data fetch 서버컴포넌트에서 보내기
@@ -107,33 +126,41 @@ export default function SearchPlayer() {
           <button type="submit">검색</button>
         </div>
       </form>
+      {isAllLoad ? null : <Loading />}
       <div className={searchModal.search_list}>
-        {searchData.map((el) => {
-          const { id, name, seasonImg, className } = el;
+        {searchData ? (
+          searchData.map((el) => {
+            const { id, name, seasonImg, className } = el;
 
-          return (
-            <div key={id} className={searchModal.search_list_player}>
-              <img
-                className={searchModal.search_list_player_img}
-                alt={name}
-                src={`https://fco.dn.nexoncdn.co.kr/live/externalAssets/common/playersAction/p${id}.png`}
-                onError={(e) => {
-                  handleImageError(e, id);
-                }}
-              />
-              <div className={searchModal.search_list_player_info}>
-                <Image
-                  width={30}
-                  height={20}
-                  className={searchModal.search_list_player_season}
-                  alt={className}
-                  src={seasonImg}
+            return (
+              <div key={id} className={searchModal.search_list_player}>
+                <img
+                  className={searchModal.search_list_player_img}
+                  alt={name}
+                  src={`https://fco.dn.nexoncdn.co.kr/live/externalAssets/common/playersAction/p${id}.png`}
+                  onError={(e) => {
+                    handleImageError(e, id);
+                  }}
+                  onLoad={() => {
+                    handleOnLoad(id);
+                  }}
                 />
-                <p className={searchModal.search_list_player_name}>{name}</p>
+                <div className={searchModal.search_list_player_info}>
+                  <Image
+                    width={30}
+                    height={20}
+                    className={searchModal.search_list_player_season}
+                    alt={className}
+                    src={seasonImg}
+                  />
+                  <p className={searchModal.search_list_player_name}>{name}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <p>검색된 데이터가 없습니다.</p>
+        )}
       </div>
     </div>
   );
